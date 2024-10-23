ea90b80220eb4f7b7de0a281cd3264d6a9436bcf
Kourosh Alasti <coding@kouroshalasti.com>
2024-10-23 03:50:04 -0700

    feat(logger): Update examples
    Updating examples for new Logger implementation

--- examples/configured.ts
+++ examples/configured.ts
@@ -1,4 +1,4 @@
-import { addColor, createLogger } from '../src/index.ts'
+import { addColorToText, createLogger } from '../src/index.ts';
 
 const coloredLogger = createLogger({
   colors: {
@@ -6,44 +6,47 @@ const coloredLogger = createLogger({
     warn: '#95B8D1',
     debug: '#B8E0D2',
     error: '#EAC4D5',
-    info: '#D6EADF'
-  }
-})
+    info: '#D6EADF',
+  },
+});
 
-coloredLogger.log('This is a Colored [LOG] Message')
-coloredLogger.warn('This is a Colored [WARN] Message')
-coloredLogger.debug('This is a Colored [DEBUG] Message')
-coloredLogger.error('This is a Colored [ERROR] Message')
-coloredLogger.info('This is a Colored [INFO] Message')
-coloredLogger.export('./examples/logs/colored')
+coloredLogger.log('This is a Colored [LOG] Message');
+coloredLogger.warn('This is a Colored [WARN] Message');
+coloredLogger.debug('This is a Colored [DEBUG] Message');
+coloredLogger.error('This is a Colored [ERROR] Message');
+coloredLogger.info('This is a Colored [INFO] Message');
+coloredLogger.export('./examples/logs/colored');
 
 const timestampLogger = createLogger({
-  timestamps: false
-})
+  timestamps: false,
+});
 
-timestampLogger.log('This is a Timestamp Off [LOG] Message')
-timestampLogger.warn('This is a Timestamp Off [WARN] Message')
-timestampLogger.debug('This is a Timestamp Off [DEBUG] Message')
-timestampLogger.error('This is a Timestamp Off [ERROR] Message')
-timestampLogger.info('This is a Timestamp Off [INFO] Message')
-timestampLogger.export('./examples/logs/timestamp-off')
+timestampLogger.log('This is a Timestamp Off [LOG] Message');
+timestampLogger.warn('This is a Timestamp Off [WARN] Message');
+timestampLogger.debug('This is a Timestamp Off [DEBUG] Message');
+timestampLogger.error('This is a Timestamp Off [ERROR] Message');
+timestampLogger.info('This is a Timestamp Off [INFO] Message');
+timestampLogger.export('./examples/logs/timestamp-off');
 
 const messageLogger = createLogger({
   messages: {
     log: ({ timestamp, level, message, color }) => {
-      const coloredLevel = addColor(color, `[${level.toUpperCase()}]`)
+      const coloredLevel = addColorToText(color, `[${level.toUpperCase()}]`);
 
-      return `This is a Custom Message ${coloredLevel} ${message} @ ${timestamp}`
+      return `This is a Custom Message ${coloredLevel} ${message} @ ${timestamp}`;
     },
     error: ({ timestamp, level, message }) => {
-      const coloredMessage = addColor('#E74C3C', message)
-      const formattedDate = new Date(timestamp)
+      const coloredMessage = addColorToText('#E74C3C', message);
+      const formattedDate = new Date(timestamp);
 
-      return `[${level.toUpperCase()}] ${coloredMessage} - ${formattedDate.getFullYear()}`
-    }
-  }
-})
+      return `[${level.toUpperCase()}] ${coloredMessage} - ${formattedDate.getFullYear()}`;
+    },
+  },
+});
 
-messageLogger.log('This a Custom [LOG] Function')
-messageLogger.error('Custom Color [ERROR]')
-messageLogger.export('./examples/logs/custom-message')
+messageLogger.log('This a Custom [LOG] Function');
+messageLogger.error('Custom Color [ERROR]');
+messageLogger.export('./examples/logs/custom-message');
+messageLogger.export('./examples/logs/customjson.json', {
+  format: 'json',
+});
--- examples/default.ts
+++ examples/default.ts
@@ -1,10 +1,10 @@
-import { createLogger } from '../src/index.ts'
+import { createLogger } from '../src/index.ts';
 
-const logger = createLogger()
+const logger = createLogger();
 
-logger.log('This is a Default [LOG] Message')
-logger.warn('This is a Default [WARN] Message')
-logger.debug('This is a Default [DEBUG] Message')
-logger.error('This is a Default [ERROR] Message')
-logger.info('This is a default [INFO] Message')
-logger.export('./examples/logs/default')
+logger.log('This is a Default [LOG] Message');
+logger.warn('This is a Default [WARN] Message');
+logger.debug('This is a Default [DEBUG] Message');
+logger.error('This is a Default [ERROR] Message');
+logger.info('This is a default [INFO] Message');
+logger.export('./examples/logs/default');
